12414e6407dc3a6ecf65943b9847c2851c106f94
codedbymayank <mayankpal7724@gmail.com>
2025-05-15 22:43:33 +0530

    Lecture - ( Classess & Objecrts )

---
+++ Lecture - ( Classess & Objecrts )/Lecture - ( Classess & Objecrts )/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_____Classess___Objecrts__
+{
+    class A
+    {
+        int somevar;
+        public int m1()
+        {
+            return 0;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A obj = new A();
+            obj.m1();
+        }
+    }
+}
