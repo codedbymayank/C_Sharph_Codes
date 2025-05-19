eccc26beb54fffa729c1d65a15d7951d4f234714
codedbymayank <mayankpal7724@gmail.com>
2025-05-19 14:48:04 +0530

    Lecture - ( Encapsultaion )

---
+++ Lecture - ( Encapsultaion )/Lecture - ( Encapsultaion )/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_____Encapsultaion__
+{
+
+    class Encap
+    {
+        private int id;
+        private string name;    
+
+        public int M1
+        {
+            set { id = value; }
+            get { return id; }
+
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Encap encap = new Encap();
+            encap.M1 = 199;
+            Console.WriteLine(encap.M1);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
