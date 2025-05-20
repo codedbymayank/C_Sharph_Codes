b49fc2055c87fac729a2a5851c6da357b9935a9c
codedbymayank <mayankpal7724@gmail.com>
2025-05-20 15:01:35 +0530

    Lecture - ( Polymorphism )

---
+++ Lecture - ( Polymorphism )/Lecture - ( Polymorphism )/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_____Polymorphism__
+{
+    //Method Overloading
+    class A
+    {
+        public void M1()
+        {
+            Console.WriteLine("Method 1");
+        }
+    }
+
+    class B
+    {
+        public void M1(string name)
+        {
+            Console.WriteLine("Method 2 : " + name);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            a.M1();
+        }
+    }
+}
---
+++ Lecture - ( Polymorphism )/Lecture - ( Polymorphism )/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lecture - ( Polymorphism )")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lecture - ( Polymorphism )")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("adebdb1d-e405-4075-8903-b3740d132617")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
