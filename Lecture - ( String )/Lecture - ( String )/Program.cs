58281d8bb98e03c9bd3ea1645cb4c2e59854726f
codedbymayank <mayankpal7724@gmail.com>
2025-05-20 10:20:02 +0530

    Lecture - ( String ) & ( StringBuilder )

---
+++ Lecture - ( String )/Lecture - ( String )/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_____String__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string st = "There is something";
+            string[] strarr = new string[] { "hello" };
+
+            Console.WriteLine(st[2]);
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Hello");
+
+            // There are so many inbuilt func which we can use in stringbuilder
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
