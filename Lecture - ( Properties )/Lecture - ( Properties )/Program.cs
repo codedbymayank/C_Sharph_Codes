5ff7f56c869e8e6270984e54128afa7654c43954
codedbymayank <mayankpal7724@gmail.com>
2025-05-20 11:25:13 +0530

    Lecture - ( Properties )

---
+++ Lecture - ( Properties )/Lecture - ( Properties )/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_____Properties__
+{
+    class A
+    {
+        public int id;
+        public string name;
+
+        public string p1
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A obj = new A();
+           
+            obj.name = "New Name";
+            Console.WriteLine(obj.p1);
+
+            Console.ReadLine();
+
+        }
+    }
+}
