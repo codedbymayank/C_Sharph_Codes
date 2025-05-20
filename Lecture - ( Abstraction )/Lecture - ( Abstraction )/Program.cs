1ca969f9751aedfec09cd1ea247837eed39f3fe2
codedbymayank <mayankpal7724@gmail.com>
2025-05-20 09:26:00 +0530

    Lecture - ( Interfaces )

--- Lecture - ( Abstraction )/Lecture - ( Abstraction )/Program.cs
+++ Lecture - ( Abstraction )/Lecture - ( Abstraction )/Program.cs
@@ -10,6 +10,7 @@ namespace Lecture_____Abstraction__
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
---
+++ Lecture - ( Interfaces )/Lecture - ( Interfaces )/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_____Interfaces__
+{
+    interface In1
+    {
+        void M1();
+        
+    }
+
+    interface In2
+    {
+        void M1();
+    }
+
+    class A:In1 , In2
+    {
+        public void M1()
+        {
+            Console.WriteLine("Interface 1 Method");
+        }
+
+        public void M2()
+        {
+            Console.WriteLine("Interface 2 Method");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A obj = new A();
+            obj.M1();
+            obj.M2();
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
