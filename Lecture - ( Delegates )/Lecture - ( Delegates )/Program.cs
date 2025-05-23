f40d1df1e99569dda0b88e9fe6d256254bee14c7
codedbymayank <mayankpal7724@gmail.com>
2025-05-23 11:29:13 +0530

    Lecture - ( Delegates )

--- Lecture - ( Delegates )/Lecture - ( Delegates )/Program.cs
+++ Lecture - ( Delegates )/Lecture - ( Delegates )/Program.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+delegate void GreetUser(string name);
 namespace Lecture_____Delegates__
 {
+    
     internal class Program
     {
+        public void SayHello(string name)
+        {
+            Console.WriteLine("Hello, " + name + "!");
+        }
+
+        public void SayGoodbye(string name)
+        {
+            Console.WriteLine("Goodbye, " + name + "!");
+        }
         static void Main(string[] args)
         {
+            Program program = new Program();
+
+            GreetUser obj = new GreetUser(program.SayHello);
+            //Multicasting
+            obj += program.SayGoodbye;
+            obj.Invoke("Mayank");
+
         }
     }
 }
