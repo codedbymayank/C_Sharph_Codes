c3c70724e9645dd01a6518ba89915bc1b38092a1
codedbymayank <mayankpal7724@gmail.com>
2025-05-23 10:12:33 +0530

    Lecture - ( Eception Handling )

---
+++ Lecture - ( Eception Handling )/Lecture - ( Eception Handling )/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_____Eception_Handling__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            }
+            //Multiple catch can be there
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            /*
+                There are multiple type of exception 
+                1. System.Io Exception
+                2. System.ArrayMismatch Exception
+                3. System.NullRefrence Exception
+             */
+        }
+    }
+}
