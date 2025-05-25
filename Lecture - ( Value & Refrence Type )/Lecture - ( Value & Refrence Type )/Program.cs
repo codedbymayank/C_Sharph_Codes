decab85a1311287b32e6c3f5fb7f2995ec01a2c5
codedbymayank <mayankpal7724@gmail.com>
2025-05-25 14:16:59 +0530

    Lecture - ( Value & Refrence Type )

---
+++ Lecture - ( Value & Refrence Type )/Lecture - ( Value & Refrence Type )/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_____Value___Refrence_Type__
+{
+    class Refclass
+    {
+        public string name;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+
+            //Value type
+            int b = a;
+
+            //Refrence Type
+            Refclass obj = new Refclass();
+            obj.name = "Rudra";
+            Refclass obj2 = obj;
+            obj2.name = "Omkara";
+
+            Console.WriteLine("After Refrence Type");
+            Console.WriteLine(obj.name);
+            Console.WriteLine(obj2.name);
+            Console.ReadLine();
+
+
+        }
+    }
+}
