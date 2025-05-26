eb293ca7f8197de6e4b7883cbb47e20f84dd587a
codedbymayank <mayankpal7724@gmail.com>
2025-05-26 10:45:23 +0530

    Lecture - ( Method Hiding )

---
+++ Lecture - ( Method Hiding )/Lecture - ( Method Hiding )/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_____Method_Hiding__
+{
+    class pclass
+    {
+        public new void m1()
+        {
+            Console.WriteLine("Parent class method");
+        }
+    }
+
+    class cclass : pclass
+    {
+        public new  void m1()
+        {
+            Console.WriteLine("Child class method");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            cclass obj = new cclass();
+            obj.m1();
+
+            Console.ReadLine();
+
+        }
+    }
+}
